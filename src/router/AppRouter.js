a08ff8b1fa7862fdd9173ded262c820817d2d754
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-03-16 00:22:04 -0400

    added route for First Step page

---
+++ src/router/AppRouter.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import FirstStep from '../components/FirstStep';
+import Header from '../components/Header';
+
+const AppRouter = () => {
+    <Router>
+        <div className="container">
+            <Header />
+            <Switch>
+                <Route component={FirstStep} path="/" exact={true} />
+            </Switch>
+        </div>
+    </Router>
+};
+
+export const AppRouter;
