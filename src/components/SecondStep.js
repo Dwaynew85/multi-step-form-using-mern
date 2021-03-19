de6eef05795144e965d4e64a74a14afb3cf58f46
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-03-19 15:54:57 -0400

    removed second React import and added .push to onSubmit function

--- src/components/SecondStep.js
+++ src/components/SecondStep.js
@@ -2,13 +2,12 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 import { Form, Button } from 'react-bootstrap';
 
-import React from 'react'
-
 const SecondStep = (props) => {
     const { register, handleSubmit, errors } = useForm();
 
     const onSubmit = (data) => {
         console.log(data);
+        props.history.push('/third');
     }
 
     return (
