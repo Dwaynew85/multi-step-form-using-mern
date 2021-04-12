c2420ce9c93bbb809d45dea544fd36fc98cb92e0
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-04-11 23:47:20 -0400

    steps change to indicate the current step highlated based on the current link

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,25 +1,35 @@
 import React from 'react'
-import { withRouter } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 
-const Progress = () => {
+const Progress = ({ location: { pathname } }) => {
+    const isFirstStep = pathname === '/';
+    const isSecondStep = pathname === '/second'
+    const isThirdStep = pathname === '/third';
+
     return (
-        <React.Fragment> {/* same as empty tags explicit naming only required if it has keys */}
-            <div className="steps">
-                <div className="step">
-                    <div>1</div>
-                    <div>Step 1</div>
-                </div>
-                <div className="step">
-                    <div>2</div>
-                    <div>Step 2</div>
-                </div>
-                <div className="step">
-                    <div>3</div>
-                    <div>Step 3</div>
-                </div>
+        <React.Fragment>
+          <div className="steps">
+            <div className={`${isFirstStep ? 'step active' : 'step'}`}>
+              <div>1</div>
+              <div>
+                {isSecondStep || isThirdStep ? (
+                  <Link to="/">Step 1</Link>
+                ) : (
+                  'Step 1'
+                )}
+              </div>
             </div>
+            <div className={`${isSecondStep ? 'step active' : 'step'}`}>
+              <div>2</div>
+              <div>{isThirdStep ? <Link to="/second">Step 2</Link> : 'Step 2'}</div>
+            </div>
+            <div className={`${pathname === '/third' ? 'step active' : 'step'}`}>
+              <div>3</div>
+              <div>Step 3</div>
+            </div>
+          </div>
         </React.Fragment>
-    )
-}
+      );
+    };
 
 export default withRouter(Progress);
